0024369b59bca31eb719e428088e789b7dac8c1d
andrés <olivera.h.andres@gmail.com>
2023-08-06 14:16:32 -0300

    agrega login, regiester y animzaciones

--- app.js
+++ app.js
@@ -6,5 +6,8 @@ const PORT = 3030
 app.use(express.static('public'))
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')))
+app.get('/registerOk',(req,res) => res.sendFile(path.join(__dirname,'views','registerOk.html')))
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')))
 
-app.listen(PORT, () => console.log('Servidor Corriendo como una liebre en http://localhost:' + PORT))
+app.listen(PORT, () => console.log('Servidor Corriendo como liebre en http://localhost:' + PORT))
